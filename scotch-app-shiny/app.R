90c9077a50e6c0243652241af0b6fed92654e078
jennaleeb <jennaleeblumenthal@gmail.com>
2016-03-27 14:18:41 -0400

    fixed choose scotch functionality

--- scotch-app-shiny/app.R
+++ scotch-app-shiny/app.R
@@ -1,5 +1,9 @@
 library(shiny)
 library(rio)
+library(DT)
+library(xlsx)
+library(hash)
+library(dplyr)
 
 scotch_data <- import("data/scotch_data_cleaned.csv")
 scotch_data[,1] <- NULL
@@ -8,35 +12,52 @@ scotch.dist <- as.matrix(scotch.dist, labels=TRUE)
 colnames(scotch.dist) <- rownames(scotch.dist) <- scotch_data[,1]
 head(scotch.dist)
 # find the 5 most similar whiskeys to Bunnah
-bunnah_vec <- scotch.dist[20,]
+
+bunnah_vec <- scotch.dist["Bunnahabha",]
 bunnah_vec <- sort(bunnah_vec)
 bunnah_vec[2:6]
 bunnah_frame <- as.data.frame(bunnah_vec[2:6])
-scotch_data.name <- scotch_data$V2
+colnames(bunnah_frame) <- "Closest Scotches to Bunnahabha"
+row.names(bunnah_frame)
+
+# Make a function
+
+closestScotch <- function(scotch_name) {
+  scotch_vec <- scotch.dist[scotch_name,]
+  scotch_vec <- sort(scotch_vec)
+  scotch_frame <- as.data.frame(scotch_vec[2:6])
+  colnames(scotch_frame) <- paste("Distance to", scotch_name)
+  round(scotch_frame,3)
+}
+
+key <- read.xlsx2("scotch_key.xlsx", 1, startRow = 1)
+names(key)
+col.ids <- names(scotch_data)
+col.ids[2:69]
+hash(col.ids[2:69], names(key))
+
+# figure out how to get colnames w/ 1's from dataset
+
 ui <- fluidPage(
-  h1("A Nearest-Neighbour Analysis of Whiskey"),
+
+  h1("A Nearest-Neighbour Analysis of Whiskey", align = "center"),
   
   # widget to choose scotch
   # output to display table and metric
   
-  h2("Nearest-Neighbour"),
   fluidRow(
-    column(4,
-           selectInput('name', 'Choose a favourite scotch', c(Choose='', scotch_data.name), selectize=TRUE)),
-    
-    column(1,
-           actionButton('choose', "Go")
+    column(8, offset = 4,
+           selectInput('name', ' ', c('Choose a favourite scotch'='', scotch_data.name), selectize=TRUE, width = '25em')
+           )
     ),
-    
-    
-    
-    column(4,
-           dataTableOutput('tableNeigbour'))
-    ),
-    
   
-  
-  
+  fluidRow(
+    column(4, offset = 2,
+           dataTableOutput('tableNeigbour')), 
+    
+    column(6,
+           dataTableOutput('tableNeigbourr'))
+  ),
   
   h2("Raw Data"),
   dataTableOutput('tableRaw')
@@ -45,9 +66,14 @@ ui <- fluidPage(
 
 server <- function(input, output){
   output$tableNeigbour = renderDataTable(
-    bunnah_frame,
+    if(!is.null(input$name)) {
+      closestScotch(input$name)
+    },
+
     options = list(searching = FALSE, paging = FALSE)
+
   )
+  
   
   output$tableRaw = renderDataTable({
     scotch_data
